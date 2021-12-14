fbb129884f4cb90f608f95dd538b2bb862d69cb7
jbavadiya <jignesha.bavadiya@gmail.com>
2021-12-14 17:01:21 -0500

    Added mvc to app

--- Webgentle.Bookstore/Webgentle.Bookstore/Startup.cs
+++ Webgentle.Bookstore/Webgentle.Bookstore/Startup.cs
@@ -23,6 +23,7 @@ namespace Webgentle.Bookstore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +46,7 @@ namespace Webgentle.Bookstore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
